014e39f15d54404f261dafc00b55d827080a1e70
Anthony <Kain235@gmail.com>
2022-04-26 11:30:48 -0400

    commit tree d048676421cab8e166af5826800d111d996fc2dd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array){
+    let item = array.find(item => item.result === "W" )
+    if (item === undefined){
+        return item;
+    } else
+        return item.year;
+    
+}
